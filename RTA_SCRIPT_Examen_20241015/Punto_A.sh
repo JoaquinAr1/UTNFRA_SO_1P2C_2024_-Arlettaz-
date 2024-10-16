4e7a54d7168db21fd7d00ce00d12e691c8beb8e3
JoaquinAr1 <arlettaz.joaquin@gmail.com>
2024-10-15 23:08:12 -0400

    Finalizando parcial

---
+++ RTA_SCRIPT_Examen_20241015/Punto_A.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo mkdir -p /Examenes-UTN/{alumno1,alumno_2,alumno_3}/parcial{1..3} /Examenes-UTN/profesores
---
+++ RTA_SCRIPT_Examen_20241015/Punto_F.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+echo "IP Pública:" > /home/osboxes/repogit/UTNFRASO_1P2C_2024-Arlettaz-/RTAARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
+curl -s ifconfig.me >> /home/osboxes/repogit/UTNFRA_SO_1P2C_2024-Arlettaz-/RTAARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
+
+echo -e "\nObteniendo URL del repositorio remoto..." >> /home/osboxes/repogit/UTNFRA_SO_1P2C_2024-Arlettaz-/RTAARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
+git remote get-url origin >> /home/osboxes/repogit/UTNFRA_SO_1P2C_2024-Arlettaz-/RTAARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
+
+echo -e "\nEl hash de mi usuario es:" >> /home/osboxes/repogit/UTNFRA_SO_1P2C_2024-Arlettaz-/RTAARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
+git config --global user.email | md5sum | awk '{print $1}' >> /home/osboxes/repogit/UTNFRA_SO_1P2C_2024-Arlettaz-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
